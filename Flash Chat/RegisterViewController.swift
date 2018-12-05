e1857af994761a352962e61749eff6bde1e0d057
Amarjit Singh <b.singh.amarjit@gmail.com>
2018-12-04 16:51:35 -0800

    final UI changes

--- Flash Chat/RegisterViewController.swift
+++ Flash Chat/RegisterViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 import Firebase
-
+import SVProgressHUD
 
 class RegisterViewController: UIViewController {
 
@@ -29,7 +29,7 @@ class RegisterViewController: UIViewController {
 
   
     @IBAction func registerPressed(_ sender: AnyObject) {
-        
+        SVProgressHUD.show()
 
         
         //TODO: Set up a new user on our Firbase database
@@ -41,7 +41,7 @@ class RegisterViewController: UIViewController {
             }
             else{
                 print("Registration Successful!")
-                
+                SVProgressHUD.dismiss()
                 self.performSegue(withIdentifier: "goToChat", sender: self)
             }
         }
